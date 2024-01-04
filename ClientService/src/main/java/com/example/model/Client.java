bd1f9099f4e32da4d156c81beaf9719830b4d7e9
hcs <hannahemcs@gmail.com>
2024-01-04 15:03:44 +0100

    create client object

---
+++ ClientService/src/main/java/com/example/model/Client.java
@@ -0,0 +1,37 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
